d806d71d63e465bf3fd865f253163c370ed46288
Mohammad Dahamshi <mohammad.dahamshi@gmail.com>
2025-07-20 13:58:21 +0300

    App: adds a check if copyrgiht is already added

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useState, useEffect } from 'react';
 import { addCopyRight, generateID, getRandomColor } from '@sarawebs/sb-utils';
 import Resume from './components/Resume.jsx';
 import './App.css';
-
+let copyRightAdded = false;
 function App() {
   useEffect(() => {
+    if (copyRightAdded) return;
+    copyRightAdded = true;
     addCopyRight({ title: 'SaraCV' });
   }, []);
   return (
